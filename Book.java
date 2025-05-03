69123695555a6ef71aabf5c97161cb67e6385cff
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-03 17:07:58 -0300

    feat: added Book class

---
+++ Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublication;
+
+    public Book(String title, String author, int yearPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+}
