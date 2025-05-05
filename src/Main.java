725e970442f1c2e5173a2254d931f67abb27753d
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-04 23:51:27 -0300

    feat: added UserInterface

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,80 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        LinkedList Personalizada
-//        LinkedListBook library = new LinkedListBook();
-//
-//        library.append(new Book("Clean Code", "Robert C. Martin", 2004));
-//        library.append(new Book("Clean Architecture", "Robert C. Martin", 2005));
-//        library.append(new Book("Baghavad Geeta", "Krishna", 1999));
-//
-//        library.printList();
+        List<Book> library = new LinkedList<Book>();
+        QueueBooks collectedBooks = new QueueBooks();
 
-        List<Book> library = new LinkedList<>();
+        library.add(new Book("Dom Quixote de La Mancha", "Miguel de Cervantes", 1665));
+        library.add(new Book("Metamorfose", "Franz Kafka", 1915));
+        library.add(new Book("Ensaio Sobre a Cegueira", "José Saramago", 1995));
+        library.add(new Book("Crime e Castigo", "Fiódor Dostoiévsky", 1886));
+        library.add(new Book("Nineteen Eighty-Four", "George Orwell", 1949));
+        library.add(new Book("Por Quem os Sinos Dobram", "Ernest Hemingway", 1940));
+        library.add(new Book("O Sol é Para Todos", "Harper Lee", 1960));
+        library.add(new Book("Os Miseráveis", "Victor Hugo", 1862));
+        library.add(new Book("A História de uma Serva", "Margaret Atwood", 1895));
+        library.add(new Book("Romeu E Julieta", "William Shakespeare", 1597));
 
-        library.add(new Book("Clean Code", "Robert C. Martin", 2004));
-        library.add(new Book("Clean Architecture", "Robert C. Martin", 2005));
-        library.add(new Book("Baghavad Geeta", "Krishna", 1999));
+        String continuosLoop = String.valueOf('S');
 
-        System.out.println("Lista de livros:");
-        int i = 1;
-        for (Book book : library) {
-            System.out.println(i + ". " + book);
-            i++;
+        while (continuosLoop.equals("S")) {
+            UserInterface prompt = new UserInterface();
+
+            prompt.chooseOption();
+
+            switch (prompt.getOption()) {
+                case 1: {
+                    int i = 1;
+                    for (Book book : library) {
+                        System.out.println(i + ". " + book);
+                        i++;
+                     }
+
+                    System.out.println("\n");
+
+                    break;
+                }
+
+                case 2: {
+                    int indexBook = prompt.chooseIndexBook();
+
+                    if (indexBook < 0 || indexBook > library.size()) {
+                        System.out.println("Livro não encontrado.");
+
+                        return;
+                    }
+                    Book book = library.get(indexBook - 1);
+
+                    collectedBooks.enqueue(book);
+
+                    System.out.println("Livro adicionado ao acervo.");
+
+                    collectedBooks.printQueueBooks();
+
+                    break;
+                }
+
+                case 3: {
+
+                    break;
+                }
+                case 4: {
+                    continuosLoop = "N";
+
+                    break;
+                }
+
+                default: {
+                    prompt.printInvalidOption();
+
+                    break;
+                }
+            }
+
+            if (prompt.getOption() != 4) {
+                continuosLoop = prompt.askContinuosLoop();
+            }
+
         }
     }
 }
---
+++ src/UserInterface.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    int option;
+
+    public UserInterface() {
+        this.option = 0;
+    }
+
+    void chooseOption() {
+        System.out.println("\n---------------------BIBLIOTECA VIRTUAL---------------------\n");
+
+        System.out.println("1. Listar livros.");
+        System.out.println("2. Adicionar livro para acervo.");
+        System.out.println("3. Navegar histórico de ações.");
+        System.out.println("4. Sair");
+
+        System.out.print("Escolha sua opção: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        this.option = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("\n");
+    }
+
+    int chooseIndexBook() {
+        System.out.print("Informe o índex do livro para fazer o acervo: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    void printInvalidOption() {
+        System.out.println("Opção inválida!");
+    }
+
+    String askContinuosLoop() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Desejar continuar (S/N): ");
+
+        return scanner.nextLine();
+    }
+
+    int getOption() {
+        return this.option;
+    }
+
+}
