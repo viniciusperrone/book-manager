8847cbc6c950324396ca3c3fa988d1b99a6b5cb1
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-05 21:35:21 -0300

    feat: adjusted Main

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         TreeBooks library = new TreeBooks();
         QueueBooks collectedBooks = new QueueBooks();
         StackNavigationSystem navigation = new StackNavigationSystem();
+        GraphBook bookGraph = new GraphBook();
 
         library.insert(new Book("Dom Quixote de La Mancha", "Miguel de Cervantes", 1665, "Romance", 1033));
         library.insert(new Book("Metamorfose", "Franz Kafka", 1915, "Literatura", 96));
@@ -16,6 +17,15 @@ public class Main {
         library.insert(new Book("Os Miseráveis", "Victor Hugo", 1862, "Romance", 1511));
         library.insert(new Book("A História de uma Serva", "Margaret Atwood", 1895, "Romance", 352));
         library.insert(new Book("Romeu E Julieta", "William Shakespeare", 1597, "Romance", 256));
+
+        for (Book book : library.listBooks()) {
+            bookGraph.addBook(book);
+        }
+
+        bookGraph.connectBooks(library.getBookByTitle("Dom Quixote de La Mancha"), library.getBookByTitle("Ensaio Sobre a Cegueira"));
+        bookGraph.connectBooks(library.getBookByTitle("Ensaio Sobre a Cegueira"), library.getBookByTitle("Crime e Castigo"));
+        bookGraph.connectBooks(library.getBookByTitle("Crime e Castigo"), library.getBookByTitle("Metamorfose"));
+        bookGraph.connectBooks(library.getBookByTitle("Nineteen Eighty-Four"), library.getBookByTitle("A História de uma Serva"));
 
         String continuosLoop = String.valueOf('S');
 
@@ -41,7 +51,7 @@ public class Main {
                 }
 
                 case 2: {
-                    int indexBook = prompt.chooseIndexBook();
+                    int indexBook = prompt.chooseIndexBook("Informe o índex do livro para fazer o acervo:");
 
                     if (indexBook < 0 || indexBook > bookList.size()) {
                         System.out.println("Livro não encontrado.");
@@ -66,7 +76,29 @@ public class Main {
 
                     break;
                 }
+
                 case 4: {
+                    int i = 1;
+                    for (Book book : bookList) {
+                        System.out.println(i + ". " + book);
+                        i++;
+                    }
+
+                    int indexBook = prompt.chooseIndexBook("Informe o índex do livro para fazer a recomendação:");
+
+                    Book selectedBook = bookList.get(indexBook - 1);
+
+                    if (selectedBook != null) {
+                        bookGraph.printRecommendations(selectedBook);
+                        navigation.stackItem("Acessou recomendações a partir de " + selectedBook.getTitle());
+                    } else {
+                        System.out.println("Livro inválido.");
+                    }
+
+                    break;
+                }
+
+                case 5: {
                     continuosLoop = "N";
 
                     break;
